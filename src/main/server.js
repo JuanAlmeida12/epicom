39f3b11b3593ae4469611f358d03df7baf00e46e
Juan Almeida <juan.satyro@gmail.com>
2018-12-16 22:17:54 -0300

    Add server and env configs

---
+++ src/main/server.js
@@ -0,0 +1,18 @@
+const config = require('config')
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./middlewares/cors')
+
+console.log(process.env.NODE_ENV)
+const PORT = config.get('server_port')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+server.listen(PORT, function() {
+    console.log(`Server running at port: ${PORT}`)
+})
+
+module.exports = server
